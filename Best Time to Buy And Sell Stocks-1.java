60a7a20a059c63f7545177aa9cc2b42a4dee775c
MANISH WALIA <78153901+manish15walia@users.noreply.github.com>
2023-03-09 17:02:15 +0530

    Create Best Time to Buy And Sell Stocks-1.java

---
+++ Best Time to Buy And Sell Stocks-1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+         Scanner scn=new Scanner(System.in);
+        int n = scn.nextInt(); 
+         int[] arr=new int[n];
+           for (int i = 0; i < n; i++) 
+            arr[i]=scn.nextInt();
+            System.out.print(maxProfit(arr)); 
+    }
+      public static int maxProfit(int prices[]) {
+      int lsf = Integer.MAX_VALUE;
+        int op = 0;
+        int pist = 0;
+        
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(op < pist){
+                op = pist;
+            }
+        }
+        return op;
+    }
+}
